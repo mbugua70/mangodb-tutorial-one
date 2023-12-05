cd9633a54ab3f93193901c02776abd3e82345036
john mbugua <johnymbugua1997@gmail.com>
2023-12-05 20:57:01 +0300

    additional changes

--- app.js
+++ app.js
@@ -1,10 +1,122 @@
-const {MongoClient} = require('mongodb')
-const uri = require('./atlas_uri')
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { connectToDb, getDb } = require("./atlas_uri");
 
-console.log(uri)
+const app = express();
+app.use(express.json());
 
-// initiating connection with the database
+// db connection
+let db;
+const PORT = 3000;
+connectToDb((err) => {
+  if (!err) {
+    app.listen(PORT, () => {
+      console.log(`Listening to port ${PORT}...`);
+    });
+    db = getDb();
+  }
+});
 
-const client = new MongoClient(uri)
+app.get("/books", (req, res) => {
+  let books = [];
+  db.collection("books")
+    .find() // return what is called a cursor which points to the collection of all doc
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json({ success: true, data: books });
+    })
+    .catch(() => {
+      res.status(500).json({ success: false, msg: "Could not fetch the data" });
+    });
+});
 
-// connecting to the database
+// single document using id/ params
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        if (!doc) {
+          res
+            .status(200)
+            .json({ success: true, msg: "Could not find the doc id" });
+        } else {
+          res.status(200).json({ success: true, data: doc });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ success: false, msg: "Could not fetch the data" });
+      });
+  } else {
+    res
+      .status(500)
+      .json({ success: false, msg: "The id fetched is not a valid id" });
+  }
+});
+
+// post request
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(201).json({ success: true, data: result });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ success: false, err: "Could not add a new document" });
+    });
+});
+
+// delete request
+
+app.delete("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: new ObjectId(req.params.id) })
+      .then((testOne) => {
+        res.status(200).json({
+          success: true,
+          data: testOne,
+          msg: "Doc deleted successful",
+        });
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ success: false, err: "Could not delete the doc" });
+      });
+  } else {
+    res.status(500).json({ success: false, msg: "Not a valid doc id" });
+  }
+});
+
+// patch request
+app.patch("/books/:id", (req, res) => {
+  const update = req.body;
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: update })
+      .then((updated) => {
+        res.status(200).json({
+          success: true,
+          data: updated,
+          msg: "Doc updated successful",
+        });
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ success: false, err: "Could not update the doc" });
+      });
+  } else {
+    res.status(500).json({ success: false, msg: "Not a valid doc id" });
+  }
+});
+// toArray - this one fetches all doc that the cursor points to and assign them to a new array.
--- atlas_uri.js
+++ atlas_uri.js
@@ -1,2 +1,23 @@
-module.exports = uri =
-"mongodb+srv://john:<johndoe1>@cluster0.7zupubn.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient } = require("mongodb");
+
+let dbConnect;
+
+module.exports = {
+  // initialy connect to the dtbase
+  connectToDb: (cb) => {
+    // connect () - will take the arguement as the connection string which is lke a special url connection to the dtbase
+    // local database
+    MongoClient.connect("mongodb://localhost:27017/bookstore")
+      .then((client) => {
+        dbConnect = client.db();
+        return cb();
+      })
+      .catch((err) => {
+        console.log(err);
+        return cb(err);
+      });
+  },
+  //   return the db after we have connect to it
+
+  getDb: () => dbConnect,
+};
